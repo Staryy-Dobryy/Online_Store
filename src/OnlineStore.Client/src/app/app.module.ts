29cfb2c6b92c2d06ec11b10414dde798065cb0d4
Staryy-Dobryy <lfcega1@gmail.com>
2024-02-25 23:56:01 +0200

    Added translate module to project

--- src/OnlineStore.Client/src/app/app.module.ts
+++ src/OnlineStore.Client/src/app/app.module.ts
@@ -1,7 +1,9 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +11,14 @@ import { HomeComponent } from './Components/Home/home.component';
 import { CheckoutComponent } from './Components/Checkout/checkout.component';
 import { ProductComponent } from './Components/Product/product.component';
 import { ProductsComponent } from './Components/Products/products.component';
+import { BreadcrumbsComponent } from './Components/Bredcrumbs/breadcrumbs.component';
+import { NavigationComponent } from './Components/Navigation/navigation.component';
 
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -17,9 +26,25 @@ import { ProductsComponent } from './Components/Products/products.component';
     HomeComponent,
     CheckoutComponent,
     ProductComponent,
-    ProductsComponent
+    ProductsComponent,
+    BreadcrumbsComponent,
+    NavigationComponent
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en'
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
