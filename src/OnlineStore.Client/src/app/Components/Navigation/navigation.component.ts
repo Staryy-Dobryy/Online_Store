93f84ec4a7f1f6225e36812893b92c1c4893fbf3
Staryy-Dobryy <lfcega1@gmail.com>
2024-02-28 23:06:23 +0200

    Updated navigation component. Template update. Added tracking of some control component.

--- src/OnlineStore.Client/src/app/Components/Navigation/navigation.component.ts
+++ src/OnlineStore.Client/src/app/Components/Navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit } from '@angular/core';
 import { BreadcrumbsService } from '../../Services/breadCrumbs.service';
 import { Observable } from 'rxjs';
 import { BreadcrumbsModel } from '../../Models/breadcrumbs.model';
@@ -10,18 +10,52 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
-  authorized: boolean = false
-  currentLang!: string
+export class NavigationComponent implements OnInit, AfterViewInit {
+  @Input() controlComponent!: ElementRef | undefined;
+
+  authorized: boolean = false;
+  hideNavigation: boolean = false;
+  hideNavigationCheched: boolean = false;
+
+  currentLang!: string;
 
   constructor(private router: Router, private translateService: TranslateService) { }
 
   ngOnInit(): void {
     this.currentLang = this.translateService.currentLang
-    console.log(this.router.url == "/")
     if (localStorage.getItem("jwt")) {
       this.authorized = true
     }
+    setTimeout(() => {
+      if (this.controlComponent) {
+        this.hideNavigation = true
+
+        setInterval(() => {
+          const threshold = 0.2; // Процент видимости элемента
+          const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+
+                this.hideNavigation = true
+                observer.disconnect(); // Отключите наблюдение, если необходимо
+              }
+              else {
+
+                this.hideNavigation = false
+              }
+            });
+          }, { threshold });
+          observer.observe(this.controlComponent!.nativeElement);
+
+        }, 200)
+      }
+
+      this.hideNavigationCheched = true
+    })
+  }
+
+  ngAfterViewInit(): void {
+    
   }
 
   changeLanguage(lang: string) {
